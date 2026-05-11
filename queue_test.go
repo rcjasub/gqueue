378f9bb47360ce0bda878613d831d3515b2a0d99
Jocsan Rodriguez <jasub270@gmail.com>
2026-05-11 02:19:32 -0400

    added benchmark for dequeue

--- queue_test.go
+++ queue_test.go
@@ -13,5 +13,19 @@ func BenchmarkEnqueue(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		q.Enqueue(ctx, job)
-	} 
+	}
+}
+
+func BenchmarkDequeue(b *testing.B) {
+	q := newQueue("bench")
+	ctx := context.Background()
+	job := newJob("1", "bench-job", "payload")
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		q.Enqueue(ctx, job)
+		b.StartTimer()
+		q.Dequeue(ctx)
+	}
 }
